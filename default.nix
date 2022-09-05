6f7f5e1579765b02912f0df6bb8713cbbee9cc90
Frédéric Menou <frederic.menou@gmail.com>
2022-09-05 19:07:29 +0200

    Package public/ directory in the nix derivation
    This will let us deploy assets along the binary.

--- default.nix
+++ default.nix
@@ -1,2 +1,9 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc902" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./release-bot.nix { }
+let releaseBot = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./release-bot.nix { };
+ in releaseBot.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs or [] ++ [ nixpkgs.coreutils ];
+      postInstall = oldAttrs.postInstall or "" + ''
+        mkdir -p $out/share/assets
+        cp -R $src/public/* $out/share/assets/
+      '';
+    })
